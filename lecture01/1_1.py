1b43f54bc04710cb2251b863059dc14f99ccb50b
shakhnovapolina <43180481+shakhnovapolina@users.noreply.github.com>
2023-03-16 11:26:30 +0100

    add homework1

---
+++ lecture01/1_1.py
@@ -0,0 +1,9 @@
+# 1
+print('Hello world!')
+# 2
+user_name = input("Let's enter the username: ")
+print(f"Hello {user_name}!")
+# 3
+a = int(input("Let's enter variable a: "))
+b = int(input("Let's enter variable b: "))
+print(f"Результат a * b = {a * b}")

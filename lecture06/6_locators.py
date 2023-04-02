13ca8d90b66bc00bcd813146bd85b30c209020bd
shakhnovapolina <43180481+shakhnovapolina@users.noreply.github.com>
2023-04-02 19:48:08 +0200

    add homework6

---
+++ lecture06/6_locators.py
@@ -0,0 +1,14 @@
+div:last-child button:nth-child(3)
+
+
+//div[2]/button[last()-1]
+/html/body/div[2]/button[3]
+//div[2]/button[3]
+//div/button[text()='Gold']
+
+
+
+
+____________
+//div/*[text()='Fully charged cat']
+/html/body/main/div/div/div/div[5]/div/div/p
